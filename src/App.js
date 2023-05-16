e235130be19d9e8cbe5b916103753c229e189657
Abhi140501 <56508923+Abhi140501@users.noreply.github.com>
2023-05-16 12:28:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         My Products
+        react product
         </p>
         <a
           className="App-link"
