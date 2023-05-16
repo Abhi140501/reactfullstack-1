bfe929c5e29789a9770c5bbec33390033e6e1bdc
Abhi140501 <abhi140501@gmail.com>
2023-05-16 12:32:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          REACT IS FUN
         </a>
       </header>
     </div>
